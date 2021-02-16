46ac05446e818d75bc4fe9a522a95f62cb7c76a9
Duncalicious <dbru997@gmail.com>
2021-02-15 19:41:25 -0700

    lib set up

---
+++ optimized_ackermann.rb
@@ -0,0 +1,28 @@
+
+
+# ackermann (0,0) is 1
+# ackermann (0,1) is 2
+# ackermann (0,2) is 3
+# ackermann (0,3) is 4
+# ackermann (0,4) is 5
+# ackermann (0,5) is 6
+# ackermann (1,0) is 2
+# ackermann (1,1) is 3
+# ackermann (1,2) is 4
+# ackermann (1,3) is 5
+# ackermann (1,4) is 6
+# ackermann (1,5) is 7
+# ackermann (2,0) is 3
+# ackermann (2,1) is 5
+# ackermann (2,2) is 7
+# ackermann (2,3) is 9
+# ackermann (2,4) is 11
+# ackermann (2,5) is 13
+# ackermann (3,0) is 5
+# ackermann (3,1) is 13
+# ackermann (3,2) is 29
+# ackermann (3,3) is 61
+# ackermann (3,4) is 125
+# ackermann (3,5) is 253
+# ackermann (4,0) is 13
+# ackermann (4,1) is 65,533
